cb56745e261fa0daae035efc861aa439b356b9ff
03685556231 <03685556231@JIPA-LABIN03-06.ifro.local>
2018-05-14 13:16:09 -0400

    instalando JFoenix.

--- grupo3/src/br/edu/ifro/menuPrincipalController.java
+++ grupo3/src/br/edu/ifro/menuPrincipalController.java
@@ -5,13 +5,18 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -25,6 +30,9 @@ public class menuPrincipalController implements Initializable {
     
     @FXML
     private Button btnSoma;
+     
+    @FXML
+    private Label label;
     
     @FXML
     private void Soma(ActionEvent event) {
@@ -39,5 +47,28 @@ public class menuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+        
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(),900,682);
+            Stage stage = new Stage();
+            stage.setTitle("Cadastrar aluno");
+            stage.setScene(scene);
+            stage.show();
+}
+         catch(IOException e){
+
+}
+        
+    }
     
 }
